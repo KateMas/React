ce333e056e4d0a9eae70a2110ed43e9c2ec60cae
KateMas <e.masyuta@gmail.com>
2017-01-11 10:36:13 +0200

    added comments-block

--- app/pages/PageBlog.js
+++ app/pages/PageBlog.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import firebase from 'firebase';
 
 import Header from './../Header';
 import Footer from './../Footer';
+import CommentsForm from './../comments/CommentsForm';
+import CommentsDisplay from './../comments/CommentsDisplay';
 
 class PageBlog extends React.Component {
     render () {
@@ -18,6 +21,11 @@ class PageBlog extends React.Component {
                         <div className="wrapper-about page-blog">
                             <img src="images/features/image2.jpg" />
                             <p>Quisque diam lorem interdum vitaapibus ac scelerisque vitae pede. Donec eget tellus non erat lacinia fertum. Donec in velit vel ipsum auctovinar. Proin umcorper urna et felisstibulum iaculis lacinia est. Proin dictum elem entum velit fusce euismod. Aenean commodo ligula eget dolor. Aenean massa. Lorem ipsum dolor sit amet, consec tetuer adipis elit, aliquam eget nibh etlibura. Lorem ipsum dolor sitamet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.</p>
+                        </div>
+                        <div className="comments">
+                            <h2>You can leave a comment</h2>
+                            <CommentsForm/>
+                            <CommentsDisplay/>
                         </div>
                     </div>
                 </section>
